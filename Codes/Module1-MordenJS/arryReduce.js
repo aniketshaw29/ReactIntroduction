aa87a81aeee53e80ad229e565f2c34a05fec502b
Aniket Shaw <85289910+aniketshaw29@users.noreply.github.com>
2022-07-16 19:21:11 +0530

    added .reduce

--- Codes/Module1-MordenJS/arryReduce.js
+++ Codes/Module1-MordenJS/arryReduce.js
@@ -1,5 +1,22 @@
 const arr = [11,22,33,44]
 
-arr.reduce(function(prevVal, curVal){
+//.reduce return a single value
+//it traverse the array
 
+//possible parameters
+//previousValue
+//currentValue
+//Array
+
+//Here 0 is the initial value of accumulator during the 1st iteration
+
+// arr.reduce(function(accumulator, curVal,){
+//     console.log(accumulator,curVal)
+// },0)
+
+
+const total = arr.reduce(function(accumulator, curVal,){
+    return accumulator+curVal
 },0)
+
+console.log(total)
